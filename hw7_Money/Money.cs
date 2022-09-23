39718cb2af8a818b738d1e1ca56e04d87c976511
Antysya <antysya@mail.ru>
2022-09-23 17:22:44 +0300

    Добавьте файлы проекта.

---
+++ hw7_Money/Money.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace hw7_Money
+{
+    internal class Money
+    {
+        public int Rub { get; set; }
+        public int Kop { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Rub} руб. {Kop} коп.";
+        }
+
+        public static Money operator +(Money m, Money n)
+        {
+            Money sum = new Money
+            {
+                Rub = m.Rub + n.Rub,
+                Kop = m.Kop + n.Kop
+            };
+            if (sum.Kop > 100)
+            {
+                sum.Rub++;
+                sum.Kop -= 100;
+            }
+            return sum;
+        }
+
+        public static Money operator -(Money m, Money n)
+        {
+            Money dif = new Money
+            {
+                Kop = ((m.Rub * 100 + m.Kop) - (n.Rub * 100 + n.Kop)) % 100,
+                Rub = ((m.Rub * 100 + m.Kop) - (n.Rub * 100 + n.Kop)) / 100
+            };
+            return dif;
+        }
+
+        public static Money operator /(Money m, int x)
+        {
+            Money rez = new Money
+            {
+                Kop = (m.Rub * 100 + m.Kop) / x % 100,
+                Rub = (m.Rub * 100 + m.Kop) / x / 100
+
+            };
+            return rez;
+        }
+
+
+
+        public static Money operator *(Money m, int x)
+        {
+            Money rez = new Money
+            {
+                Kop = m.Kop * x,
+                Rub = m.Rub * x
+            };
+            if (rez.Kop > 100)
+            {
+                rez.Rub += (rez.Kop - (rez.Kop % 100)) / 100;
+                rez.Kop %= 100;
+            }
+            return rez;
+        }
+        public static Money operator ++(Money m)
+        {
+            m.Kop++;
+            if (m.Kop > 99)
+            {
+                m.Rub++;
+                m.Kop = 0;
+            }
+            return m;
+        }
+
+        public static Money operator --(Money m)
+        {
+            m.Kop--;
+            if (m.Kop < 0)
+            {
+                m.Rub--;
+                m.Kop = 99;
+            }
+            return m;
+        }
+        public override bool Equals(object obj)
+        {
+            return this.ToString() == obj.ToString();
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        public static bool operator ==(Money m1, Money m2)
+        {
+            return m1.Equals(m2);
+        }
+        public static bool operator !=(Money m1, Money m2)
+        {
+            return !(m1 == m2);
+        }
+        public static bool operator <(Money m1, Money m2)
+        {
+            return m1.Rub * 100 + m1.Kop < m2.Rub * 100 + m2.Kop;
+        }
+        public static bool operator >(Money m1, Money m2)
+        {
+            return !(m1 < m2) && !(m1 == m2);
+        }
+
+        public Money iniz()
+        {
+            Money m = new Money();
+            try
+            {
+                Write("рублей: ");
+                m.Rub = int.Parse(ReadLine());
+                Write("копеек: ");
+                m.Kop = int.Parse(ReadLine());
+                if (m.Kop > 99)
+                {
+                    throw new My_Exep2();
+                }
+            }
+            catch (My_Exep2 e)
+            {
+                WriteLine(e.Message);
+                Write("Введите заново\nкопеек: ");
+                m.Kop = int.Parse(ReadLine());
+            }
+            return m;
+        }
+        public Money prov(Money m)
+        {
+            try
+            {
+                if (m.Rub < 0 || m.Kop < 0)
+                {
+                    throw new My_Exep3();
+                }
+            }
+            catch (My_Exep3 e)
+            {
+                WriteLine(e.Message);
+                Write("Введите сумму заново:\n ");
+                m = new Money().iniz();
+            }
+            return m;
+        }
+    }
+
+    public class My_Exep : ApplicationException
+    {
+        public My_Exep() : base("Банкрот")
+        {
+
+        }
+
+    }
+
+    public class My_Exep2 : ApplicationException
+    {
+        public My_Exep2() : base("Сумма задана некорректно: количество копеек > 99")
+        {
+        }
+
+    }
+
+    public class My_Exep3 : ApplicationException
+    {
+        public My_Exep3() : base("Сумма задана некорректно: введено отрицательное значение")
+        {
+        }
+
+    }
+}
---
+++ hw7_Money/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace hw7_Money
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Меню:\n" +
+                    "1. Сложение двух денежных сумм;\n" +
+                    "2. Вычитание одной денежной суммы из другой;\n" +
+                    "3. Деление денежной суммы на коэффициент" +
+                    "4. Умножение денежной суммы на коэффициент;\n" +
+                    "5. Увиличение суммы на одну копейку заданное количество раз\n" +
+                    "6. Уменьшение суммы на одну копейку заданное количество раз\n" +
+                    "7. Сравнение 2х сумм (<,>, =, !=)\n" +
+                    "8. Выйти из программы.\n\n");
+            while (true)
+            {
+                WriteLine("Какое действие хотите произвести:\n");
+                int pos = int.Parse(ReadLine());
+                switch (pos)
+                {
+                    case 1:
+                        {
+                            WriteLine("Сложение двух денежных сумм:\n");
+                            Write("Введите первую сумму:\n");
+                            Money m = new Money().iniz();
+                            m = m.prov(m);
+
+                            Write("Введите вторую сумму:\n ");
+                            Money n = new Money().iniz();
+                            n = n.prov(n);
+                            WriteLine($"{m} + {n} = {m + n}");
+                        }
+                        break;
+
+                    case 2:
+                        {
+                            WriteLine("Вычитание одной денежной суммы из другой:\n");
+
+                            Write("Введите первую сумму:\n");
+                            Money m = new Money().iniz();
+                            m = m.prov(m);
+
+                            Write("Введите вторую сумму:\n");
+                            Money n = new Money().iniz();
+                            n = n.prov(n);
+                            try
+                            {
+                                if (m - n < new Money { Kop = 0, Rub = 0 })
+                                {
+                                    throw new My_Exep();
+                                }
+                                WriteLine($"{m} - {n} = {m - n}");
+                            }
+                            catch (My_Exep e)
+                            {
+                                WriteLine(e.Message);
+                            }
+                            WriteLine();
+                        }
+                        break;
+
+                    case 3:
+                        {
+                            WriteLine("Деление денежной суммы на коэффициент:\n");
+                            Write("Введите сумму:\n");
+                            Money m = new Money().iniz();
+                            m = m.prov(m);
+
+                            Write("Введите коэффициент: ");
+                            int n = int.Parse(ReadLine());
+
+                            try
+                            {
+                                if (n == 0)
+                                {
+                                    throw new DivideByZeroException();
+                                }
+                                if (m / n < new Money { Kop = 0, Rub = 0 })
+                                {
+                                    throw new My_Exep();
+                                }
+                                WriteLine($"{m} / {n} = {m / n}");
+                            }
+                            catch (DivideByZeroException e)
+                            {
+                                WriteLine(e.Message);
+                            }
+                            catch (My_Exep e)
+                            {
+                                WriteLine(e.Message);
+                            }
+                            WriteLine();
+                        }
+                        break;
+
+                    case 4:
+                        {
+                            WriteLine("Умножение денежной суммы на коэффициент:\n");
+                            Write("Введите сумму:\n");
+                            Money m = new Money().iniz();
+                            m = m.prov(m);
+
+                            Write("Введите коэффициент: ");
+                            int n = int.Parse(ReadLine());
+                            try
+                            {
+                                if (m * n < new Money { Kop = 0, Rub = 0 })
+                                {
+                                    throw new My_Exep();
+                                }
+                                WriteLine($"{m} * {n} = {m * n}");
+                            }
+                            catch (My_Exep e)
+                            {
+                                WriteLine(e.Message);
+                            }
+                            WriteLine();
+                        }
+                        break;
+
+                    case 5:
+                        {
+                            WriteLine("Увиличение суммы на одну копейку заданное количество раз\n");
+                            Write("Введите сумму:\n");
+                            Money m = new Money().iniz();
+                            m = m.prov(m);
+
+                            Write("Введите количество повторений: ");
+                            int n = int.Parse(ReadLine());
+                            for (int i = 0; i < n; i++)
+                            {
+                                WriteLine(m++);
+                            }
+                            WriteLine();
+                        }
+                        break;
+
+                    case 6:
+                        {
+                            WriteLine("Уменьшение суммы на одну копейку заданное количество раз\n");
+                            Write("Введите сумму:\n");
+                            Money m = new Money().iniz();
+                            m = m.prov(m);
+
+                            Write("Введите количество повторений: ");
+                            int n = int.Parse(ReadLine());
+
+                            for (int i = 0; i < n; i++)
+                            {
+                                try
+                                {
+                                    
+                                    if (m > new Money { Kop = 0, Rub = 0 })
+                                    {
+                                        WriteLine(m--);
+                                    }
+                                    else
+                                        throw new My_Exep();
+                                }
+                                catch (My_Exep e)
+                                {
+                                    WriteLine(e.Message);
+                                }
+                            }
+                            WriteLine();
+                        }
+                        break;
+
+                    case 7:
+                        {
+                            WriteLine("Сравнение 2х сумм (<,>, =, !=)\n");
+                            Write("Введите первую сумму:\n");
+                            Money m = new Money().iniz();
+                            m = m.prov(m);
+
+                            Write("Введите вторую сумму:\n ");
+                            Money n = new Money().iniz();
+                            n = n.prov(n);
+
+                            WriteLine($"m меньше n ? - {m < n}");
+                            WriteLine($"m больше n ? - {m > n}");
+                            WriteLine($"m равно n ? - {m == n}");
+                            WriteLine($"m неравно n ? - {m != n}");
+                        }
+                        break;
+
+                    case 8:
+                        return;
+
+                    default:
+                        WriteLine("Вы выбрали несуществующий пункт меню. Выберите снова.");
+                        continue;
+                }
+            }
+        }
+    }
+}
